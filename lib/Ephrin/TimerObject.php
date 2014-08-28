0095728f102832bdd258cfecac860e76c7c6429a
ephrin <lazycommit@gmail.com>
2014-08-28 20:58:09 +0300

    ok

---
+++ lib/Ephrin/TimerObject.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of the Hotels24.ua project.
+ *
+ * (c) Hotels24.ua 2007-2014
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ephrin;
+
+/**
+ * @class TimerObject
+ *
+ * @author lazycommit <lazycommit@hotels24.ua>
+ */
+class TimerObject 
+{
+
+} 
